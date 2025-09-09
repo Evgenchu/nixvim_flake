09e172181896c736f01f507763c1900e19e36c83
Evgeni Kurtev <evgenikurtev@abv.bg>
2025-09-09 13:37:57 +0200

    small tweaks, but not full result

--- config/editor/navic.nix
+++ config/editor/navic.nix
@@ -1,39 +1,16 @@
 _: {
   plugins.navic = {
     enable = true;
+    autoLoad = true;
+    lazyLoad = {
+      enable = false;
+    };
     settings = {
       separator = "  ";
       highlight = true;
       depthLimit = 5;
       lsp = {
         autoAttach = true;
-      };
-      icons = {
-        Array = "󱃵  ";
-        Boolean = "  ";
-        Class = "  ";
-        Constant = "  ";
-        Constructor = "  ";
-        Enum = " ";
-        EnumMember = " ";
-        Event = " ";
-        Field = "󰽏 ";
-        File = " ";
-        Function = "󰡱 ";
-        Interface = " ";
-        Key = "  ";
-        Method = " ";
-        Module = "󰕳 ";
-        Namespace = " ";
-        Null = "󰟢 ";
-        Number = " ";
-        Object = "  ";
-        Operator = " ";
-        Package = "󰏖 ";
-        String = " ";
-        Struct = " ";
-        TypeParameter = " ";
-        Variable = " ";
       };
     };
   };
