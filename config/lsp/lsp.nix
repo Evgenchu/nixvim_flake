9ce4055a83ac3f84b43fd4ad80c94ea6acd17316
Evgeni Kurtev <evgenikurtev@abv.bg>
2026-04-09 12:04:54 +0200

    lsp updated

--- config/lsp/lsp.nix
+++ config/lsp/lsp.nix
@@ -7,9 +7,6 @@
     lsp-format = {
       enable = true;
     };
-    helm = {
-      enable = true;
-    };
     lsp = {
       enable = true;
       inlayHints = true;
@@ -20,19 +17,10 @@
         bashls = {
           enable = true;
         };
-        html = {
-          enable = true;
-        };
         lua_ls = {
           enable = true;
         };
         nil_ls = {
-          enable = true;
-        };
-        ts_ls = {
-          enable = true;
-        };
-        marksman = {
           enable = true;
         };
         pyright = {
@@ -47,40 +35,8 @@
         jsonls = {
           enable = true;
         };
-        helm_ls = {
-          enable = true;
-          extraOptions = {
-            settings = {
-              "helm_ls" = {
-                yamlls = {
-                  path = "${pkgs.yaml-language-server}/bin/yaml-language-server";
-                };
-              };
-            };
-          };
-        };
         yamlls = {
           enable = true;
-          extraOptions = {
-            settings = {
-              yaml = {
-                schemas = {
-                  kubernetes = "'*.yaml";
-                  "http://json.schemastore.org/github-workflow" = ".github/workflows/*";
-                  "http://json.schemastore.org/github-action" = ".github/action.{yml,yaml}";
-                  "http://json.schemastore.org/ansible-stable-2.9" = "roles/tasks/*.{yml,yaml}";
-                  "http://json.schemastore.org/kustomization" = "kustomization.{yml,yaml}";
-                  "http://json.schemastore.org/ansible-playbook" = "*play*.{yml,yaml}";
-                  "http://json.schemastore.org/chart" = "Chart.{yml,yaml}";
-                  "https://json.schemastore.org/dependabot-v2" = ".github/dependabot.{yml,yaml}";
-                  "https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json" =
-                    "*docker-compose*.{yml,yaml}";
-                  "https://raw.githubusercontent.com/argoproj/argo-workflows/master/api/jsonschema/schema.json" =
-                    "*flow*.{yml,yaml}";
-                };
-              };
-            };
-          };
         };
       };
 
@@ -140,25 +96,21 @@
   diagnostic.settings = {
     virtual_lines.only_current_line = true;
   };
-  extraPlugins = with pkgs.vimPlugins; [
-    ansible-vim
-  ];
 
   extraConfigLua = ''
     local _border = "rounded"
-
-    vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
-      vim.lsp.handlers.hover, {
-        border = _border
-      }
-    )
-
-    vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
-      vim.lsp.handlers.signature_help, {
-        border = _border
-      }
-    )
-
+    local hover = vim.lsp.handlers.hover
+    vim.lsp.handlers["textDocument/hover"] = function(err, result, ctx, config)
+      config = config or {}
+      config.border = _border
+      return hover(err, result, ctx, config)
+    end
+    local signature = vim.lsp.handlers.signature_help
+    vim.lsp.handlers["textDocument/signatureHelp"] = function(err, result, ctx, config)
+      config = config or {}
+      config.border = _border
+      return signature(err, result, ctx, config)
+    end
     require('lspconfig.ui.windows').default_options = {
       border = _border
     }
